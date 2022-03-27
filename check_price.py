8aff3500d3ebb2e214a73789576ecad3279f2b12
a1426 <66330789+a1426@users.noreply.github.com>
2022-03-27 12:30:22 -0500

    Update check_price.py

--- check_price.py
+++ check_price.py
@@ -1,14 +1,15 @@
 import requests
 class ComedConnectionError(Exception):
     pass
+site="https://hourlypricing.comed.com/api?type=currenthouraverage"
 def check_price(min_price,max_price):
     #Reaches out to the Comcast API to check the price
     try:
-        req=requests.get("https://hourlypricing.comed.com/api?type=currenthouraverage")
+        req=requests.get(site)
     except requests.exceptions.ConnectionError:
         print("There was an error in trying to reach the website")
         try:
-            req=requests.get("https://hourlypricing.comed.com/api?type=currenthouraverage")
+            req=requests.get(site)
         except:
             raise ComedConnectionError("No connection could be made with Comed.")
     price=float(req.json()[0]["price"])
