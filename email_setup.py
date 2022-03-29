b7c84c0778e44c57a752705b282e27c13a6bef0e
a1426 <66330789+a1426@users.noreply.github.com>
2022-03-28 22:26:30 -0500

    Update email_setup.py

--- email_setup.py
+++ email_setup.py
@@ -1,24 +1,37 @@
 import webbrowser
 import re
-ct=1
-signup="https://accounts.google.com/signup/v2/webcreateaccount?flowName=GlifWebSignIn&flowEntry=SignUp"
-with open("email_guide.txt") as file:
-    for line in file.readlines():
-        print(line, end='')
-        input("Press Enter to Continue")
-        if ct:
-            webbrowser.open(signup)
-            ct=0
-email=input("Enter the new email address(either the name or the full thing):")
-password=input("Enter the new password:")
-head=re.search("([a-zA-Z.]*)(@gmail.com)?",email)
-email = head.group(1)+"@gmail.com"
-input(f"To confirm, your email is {email} and your password is {password}. Enter to continue.")
-input("Finally, you must enable Less Secure Apps on your new account. The link will open after you enter.")
-webbrowser.open("https://myaccount.google.com/lesssecureapps?pli=1")
-input("Once you are done, press enter.")
-
-
-
-
-
+from json import dumps
+import os
+if __name__=="main":
+    maxi=input("Enter the maximum price for power, in cents. You will be notified if the price becomes expensive.")
+    mini=input("Enter the minimum alert price for power, in cents. You will be notified if the price becomes cheap.")
+    rec_email = input("Enter your email address.")
+    ct=1
+    signup="https://accounts.google.com/signup/v2/webcreateaccount?flowName=GlifWebSignIn&flowEntry=SignUp"
+    #This is the step by step guide for how to create a new email.
+    with open("email_guide.txt") as file:
+        for line in file.readlines():
+            print(line, end="")
+            input("Press Enter to Continue")
+            if ct:
+                webbrowser.open(signup)
+                ct=0
+    email=input("Enter the new email address(either the name or the full thing):")
+    password=input("Enter the new password:")
+    head=re.search("([a-zA-Z0-9.]*)(@gmail.com)?",email)
+    email = head.group(1)+"@gmail.com"
+    input(f"To confirm, your email is {email} and your password is {password}. Enter to continue.")
+    input("Finally, you must enable Less Secure Apps on your new account. Sign in to it and the link will open after you enter.")
+    webbrowser.open("https://myaccount.google.com/lesssecureapps?pli=1")
+    input("Once you are done, press enter.")
+    p_info={
+        "sender":email,
+        "recipient":rec_email,
+        "pswd":password,
+        "maxi":maxi,
+        "mini":mini,
+    }
+    with open("info.json","w") as file:
+        file.write(dumps(p_info))
+location = os.getcwd()
+os.system('(echo "* * * * * '+location+'/initial.py >/Users/robert/Programs/cron_log 2>&1") | crontab -')
