6f863da9ba4d4e139ef772edd08d56f31c0955a1
a1426 <66330789+a1426@users.noreply.github.com>
2022-04-02 13:16:57 -0500

    Cron works as intended.

--- email_setup.py
+++ email_setup.py
@@ -34,4 +34,4 @@ if __name__=="main":
     with open("info.json","w") as file:
         file.write(dumps(p_info))
 location = os.getcwd()
-os.system(f'echo "* * * * * cd {location}; /usr/bin/python3 {location}/send_email.py >/Users/robert/Programs/cron_log 2>&1" | crontab -')
+os.system(f'(crontab -l; echo "* * * * * cd {location}; /usr/bin/python3 {location}/send_email.py") | crontab -')
