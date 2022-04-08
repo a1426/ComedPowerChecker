6e60c4cef4a6b4f54c4fe1e6fb677c62c5cbe58a
a1426 <66330789+a1426@users.noreply.github.com>
2022-04-07 20:18:49 -0500

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -1,23 +1,32 @@
 import smtplib
-from email.message import EmailMessage
 from json import loads
 import os
-from initial import location
-#NOTE: THIS DOES NOT WORK YET. There is an error with the location, as cron changes the active directory.
-with open(location+"/info.json") as file:
-    info=(loads(file.read()))
-messages = ["The price for power is now {} the threshold at {} cents.","The power has returned to a normal price"]
-message=messages[0]
-def send_email():
-    msg = EmailMessage()
-    msg.set_content(message)
-    msg['subject'] = "Comed Power Price Alert"
-    msg['to'] = info["recipient"]
-    msg['from'] = info["sender"]
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.starttls()
-    server.login(info["sender"], info["pswd"])
-    server.send_message(msg)
-    server.quit()
-send_email()
+from check_power_price import check_price
+import sys
+with open("info.json") as file:
+    info=loads(file.read())
 
+messages = ["The price for power is now {} the threshold", "The power is at a normal price"]
+price = check_price()
+
+if price>float(info["maxi"]):
+    relative = "above"
+elif price<float(info["mini"]):
+    relative = "below"
+else:
+    relative = "in"
+relative="above"
+with open("recent_alert.txt", "r+") as f:
+    if relative == f.read():
+        pass
+    else:
+        if relative == "in":
+            message=messages[1]
+        else:
+            message=messages[0].format(relative)
+        f.seek(0)
+        f.write(relative)
+        f.truncate()
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+            server.login(info["sender"], info["pswd"])
+            server.sendmail(info["sender"], info["recipient"], message)
