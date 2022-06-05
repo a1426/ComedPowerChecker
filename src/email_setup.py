b9a5b4c462e437ce6c2dfdab2cd5796ad0d6b1a9
Robert Szwarc <2005szwarcr@gmail.com>
2022-06-05 16:01:19 -0500

    Condensed README, and broke up email_setup.py in preparation for future changes.

--- src/email_setup.py
+++ src/email_setup.py
@@ -3,10 +3,22 @@ import re
 from json import dumps
 import os
 #Prompts user for credentials
-def set_credentials(first_time):
+def set_credentials(email,rec_email,password,maxi,mini):
+    #Store user data and save it into info.json.
+    p_info={
+        "sender":email,
+        "recipient":rec_email,
+        "pswd":password,
+        "maxi":maxi,
+        "mini":mini,
+    }
+    with open("src/info.json","w") as file:
+        file.write(dumps(p_info))
+
+def promt_credentials(first_time):
     maxi=input("Enter the maximum price for power, in cents. You will be notified if the price becomes expensive.")
     mini=input("Enter the minimum alert price for power, in cents. You will be notified if the price becomes cheap.")
-    rec_email = input("Enter your email address.")
+    rec_email = input("Enter your email address. Alerts will be sent to this email.")
     #If its their first time, the user will have to be guided through setting up a disposable email.
     if first_time:
         ct=1
@@ -19,25 +31,17 @@ def set_credentials(first_time):
                 if ct:
                     webbrowser.open(signup)
                     ct=0
-    email=input("Enter the new email address(either the name or the full thing):")
-    password=input("Enter the new password:")
-    #Note: Is this regex really necessary?
-    head=re.search("([a-zA-Z0-9.]*)(@gmail.com)?",email)
-    email = head.group(1)+"@gmail.com"
+    email=input("Enter the new email address:")
+    two_factor="https://myaccount.google.com/signinoptions/two-step-verification/enroll-welcome"
+    input("Finally, you must enable two-factor authentication to get the app-password. The link will open")
+    webbrowser.open(two_factor)
+    app_psw="https://myaccount.google.com/u/1/apppasswords"
+    input("Finally, you must create an app password. Press enter to open the link. Select \"other\" as your app, and your computer")
+    webbrowser.open(app_psw)
+    password=input("Enter the app-password:")
     input(f"To confirm, your email is {email} and your password is {password}. Enter to continue. To reset, run reset_creds.py")
-    input("Finally, you must enable Less Secure Apps on your new account. Sign in to it and the link will open after you enter.")
-    webbrowser.open("https://myaccount.google.com/lesssecureapps?pli=1")
-    input("Once you are done, press enter.")
-    #Store user data and save it into info.json.
-    p_info={
-        "sender":email,
-        "recipient":rec_email,
-        "pswd":password,
-        "maxi":maxi,
-        "mini":mini,
-    }
-    with open("src/info.json","w") as file:
-        file.write(dumps(p_info)) 
+    set_credentials(email,rec_email,app_psw,maxi,mini)
+
 if __name__=="main":
     set_credentials(True)
     #Checks to make sure the connection can be authenticated
